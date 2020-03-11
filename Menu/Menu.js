bfe2fa1891283615509f9fbbd72353721f4c04a3
RobertMisch <robertmisch94@gmail.com>
2020-03-11 17:48:44 -0400

    finished mvp awhile ago

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -33,3 +33,34 @@ let menuItems = [
   Step 6: add the menu component to the DOM.
   
 */
+
+function myMenu(myArray){
+  //make elements
+  const menu = document.createElement('div');
+  const menuList = document.createElement('ul');
+
+  //make the list elements
+  myArray.forEach(item => {
+    let menuListItem = document.createElement('li');
+    menuListItem.textContent = item;
+    menuList.appendChild(menuListItem);
+  })
+  //add class to menu
+
+  menu.classList.add('menu');
+
+  //append list to menu
+  menu.appendChild(menuList)
+
+  //add action listeners
+  document.querySelector('.menu-button').addEventListener('click', function(){
+    console.log('we got ehre');
+    menu.classList.toggle('menu--open');
+    // gsap.to(".menu--open", {duration: 2, y:400});
+  })
+  return menu;
+}
+
+document.querySelector('.header').append(myMenu(menuItems));
+
+
